008a8f19d56d94506b236ec7e8afbccfe2c21147
Tomas Zagorskis <tomzagos@gmail.com>
2023-09-13 12:36:34 +0300

    feat: add projects section

---
+++ components/projects.tsx
@@ -0,0 +1,20 @@
+import { Fragment } from 'react';
+
+import { projectsData } from '@/lib/data';
+import Project from './project';
+import SectionHeading from './section-heading';
+
+export default function Projects() {
+	return (
+		<section>
+			<SectionHeading>My Projects</SectionHeading>
+			<div>
+				{projectsData.map((project, index) => (
+					<Fragment key={project.title.replace(' ', '-')}>
+						<Project {...project} />
+					</Fragment>
+				))}
+			</div>
+		</section>
+	);
+}
